97d7700b2b0c7dc1c24074e0736c11f86c91cf25
mikeprussell <mikeprussell@outlook.com>
2021-10-05 08:19:57 -0500

    Commit with notes

--- Multiples3And5/Program.cs
+++ Multiples3And5/Program.cs
@@ -8,20 +8,28 @@ namespace Multiples3And5
 {
     class Program
     {
+        //This program returns the sum of all numbers 
+        //that are multiples of 3 or 5 that are below 1,000.
+
         static void Main(string[] args)
         {
-            int sum = 0;
+            //variable tracks sum
+            int total = 0;
 
             for (int i = 0; i < 1000; i++)
             {
+                //if statement adds to sum when the remainder is 0 
+                //when divided by 3 or 5 (or condition removes duplicates)
                 if ((i % 3 == 0) || (i % 5 == 0))
                 {
-                    sum = sum + i;
+                    total = total + i;
                 }
             }
 
-            Console.WriteLine(sum);
+            Console.WriteLine(total);
 
         }
-    }
-}
+
+    }//End of class
+
+}//End of namespace
